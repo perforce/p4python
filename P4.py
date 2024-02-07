6a6ecaad417fa26d66d2417259ab1e79a41af3b1
Himanshu Jain <hjain@perforce.com>
2024-02-07 05:59:37 -0600

    2023.2 patch 1 release code drop
    Made P4Exception object pickleable.

--- P4.py
+++ P4.py
@@ -7,7 +7,7 @@ from __future__ import print_function
     This uses the Python type P4API.P4Adapter, which is a wrapper for the
     Perforce ClientApi object.
     
-    $Id: //depot/main/p4-python/P4.py#109 $
+    $Id: //depot/main/p4-python/P4.py#110 $
     
     #*******************************************************************************
     # Copyright (c) 2007-2010, Perforce Software, Inc.  All rights reserved.
@@ -64,8 +64,15 @@ class P4Exception(Exception):
             self.warnings = value[2]
         else:
             self.value = value
+
     def __str__(self):
         return str(self.value)
+
+    def __reduce__(self):
+        if hasattr(self, 'errors'):
+            return (self.__class__, ((self.value, self.errors, self.warnings),))
+        return (self.__class__, (self.value,))
+
 
 class Spec(dict):
     """Subclass of dict, representing the fields of a spec definition.
